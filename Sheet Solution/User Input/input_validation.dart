328a6ce40a5ab1f3cc4b6a97a6c32f04995bb856
abdo-essam <abdo.e.mosa@gmail.com>
2025-03-02 09:55:21 +0200

    update

--- Sheet Solution/User Input/input_validation.dart
+++ Sheet Solution/User Input/input_validation.dart
@@ -1,13 +1,13 @@
 import 'dart:io';
 
 void main() {
-  int number;
+  int? number;
 
   do {
     print('Enter a number between 1 and 10:');
     String input = stdin.readLineSync()!;
-    number = int.tryParse(input) ?? -1; // Convert input to int, default to -1 if invalid
-  } while (number < 1 || number > 10); // Repeat until valid input
+    number = int.tryParse(input); // Convert input to int, default to -1 if invalid
+  } while (number == null || number < 1 || number > 10); // Repeat until valid input
 
   print('You entered a valid number: $number');
 }
--- test.dart
+++ test.dart
@@ -1,16 +1,1 @@
-import 'dart:io';
 
-void main() {
-
-}
-
-
-
-
-// toUpperCase(), toLowerCase(): Changing case.
-// length: Getting the length of a string.
-// substring(): Extracting parts of a string.
-// indexOf(): Finding the position of a substring.
-// contains(): Checking if a string contains a substring.
-// replaceAll(): Replacing occurrences of a substring.
-// split(): Splitting a string into a list of substrings.
