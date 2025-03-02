787e85d90b6627f610e2e764d2dae0d9655f9d6b
abdo-essam <abdo.e.mosa@gmail.com>
2025-03-02 09:16:56 +0200

    update

--- Sheet Solution/Lists/list_filtering.dart
+++ Sheet Solution/Lists/list_filtering.dart
@@ -1,7 +1,7 @@
 void main() {
   List<String> words = ['apple', 'banana', 'cherry', 'date', 'elderberry','amazon'];
   String letter = 'a';
-  List<String> filteredWords = words.where((word) => word.startsWith(letter)).toList();
+  List<String> filteredWords = words.where((word) => word.toLowerCase().startsWith(letter.toLowerCase())).toList();
   print('List: $words');
   print('Words starting with "$letter": $filteredWords');
 }
